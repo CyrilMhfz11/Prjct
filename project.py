85e8a19b66c062b8d459a2b7a4be8be0607be5d1
CyrilMhfz11 <cyril.mahfouz@gmail.com>
2023-05-08 22:20:55 +0300

    Filling the log in page

--- project.py
+++ project.py
@@ -14,3 +14,18 @@ driver=webdriver.Chrome(service=service)
 
 url="https://myportal.lau.edu.lb/Pages/studentPortal.aspx"
 driver.get(url)
+
+username_input=driver.find_element("xpath",
+                                    "//input[@type='text' and @id='username' and @name='username']")
+
+password_input=driver.find_element("xpath",
+                                    "//input[@type='password' and @id='password']")
+
+button_input=driver.find_element("xpath",
+                                  "//input[@type='submit' and @value='Log In']")
+
+username_input.send_keys(username)
+password_input.send_keys(password)
+button_input.click()
+
+print("logged in")
